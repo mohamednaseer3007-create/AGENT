0b2f33a29289efe620eef9ff6a14e1d16a6c87d5
mohamednaseer3007-create <mohamednaseer3007@gmail.com>
2026-09-09 09:38:12 +0530

    Refactor Flask app initialization and routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,22 @@
-import os , urllib.parse , urllib.request , render_template
+from flask import Flask, render_template
 from app.youtube import youtube_bp
-gemini_api_key = "Gemini_API_Key"; 
 
-def home():
-  return render_template ("index.html")
-  
+
 def create_app():
-  app = Flask(_name_) 
-//appp?//
-  app.register_Blueprint(youtube_bp , url_prefix="/youtube")
-  @app.route("/html")
 
-  def html():
-    return render_template("index.html")
-  return app;
+    app = Flask(__name__)
 
+    app.register_blueprint(
+        youtube_bp,
+        url_prefix="/youtube"
+    )
 
+    @app.route("/")
+    def home():
+        return render_template("index.html")
+
+    @app.route("/html")
+    def html():
+        return render_template("index.html")
+
+    return app
