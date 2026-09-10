cacc32cde7cecee17261c1413e8771c055f4b0e6
mohamednaseer3007-create <mohamednaseer3007@gmail.com>
2026-09-10 14:18:33 +0530

    Update gmail_write.py

--- app/gmail/gmail_write.py
+++ app/gmail/gmail_write.py
@@ -1,1 +1,12 @@
+import os 
+import re 
+import urllib.parse
 
+CLIENT_EMAIL = os.getenv("CLIENT_EMAIL","")
+
+KEYWORDS = (
+  "gmail","email","e-mail","mail",
+  "write an email","send an email","draft an email0",
+  "compose an email","write maill","send mail","draft mail",
+  "compose mail"
+)
