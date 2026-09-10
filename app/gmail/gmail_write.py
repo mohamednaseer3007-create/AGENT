1e7d7b67bd064c3781bd4709ba82dabd1a4c6f9d
mohamednaseer3007-create <mohamednaseer3007@gmail.com>
2026-09-10 14:42:27 +0530

    Add email extraction and Gmail URL creation functions
    Added functions to extract email addresses from text and create a Gmail URL.

--- app/gmail/gmail_write.py
+++ app/gmail/gmail_write.py
@@ -14,3 +14,28 @@ KEYWORDS = (
 def is_email_command(test):
   text = text.lower()
   return any(k in text for k in KEYWORDS)
+
+def exact_email(text):
+  match = re.search(r"[\w.+-]+@[\w.-]+\.\w+",text)
+  if match :
+    return match.group(0)
+
+match = re.search(
+  r"([\w.+-]+)\s+at\s+([\w.-]+)\s+dot\s+(\w+)"
+  text.lower()
+)
+if match:
+  return f"{match.group(1)}@{match.group(2)}.{match.group(3)}"
+
+  return CLIENT_EMAIL
+  return ""
+
+def create_gmail_url(subject ="", body="", recipient="")
+  params = urllib.parse.urlencode({
+    "view": "cm",
+    "fs":"1"
+    "to":recipient,
+    "su":subject,
+    "body":body
+  })
+  return f"https://mail.goole.com/mail/u/0/?{params}"
