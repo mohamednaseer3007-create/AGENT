f6a7d0908c543be70052c55abf1cc050b1b55966
mohamednaseer3007-create <mohamednaseer3007@gmail.com>
2026-09-10 15:53:35 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -38,3 +38,48 @@ def html():
 def health():
     return jsonify({
         "status": "ok"
+        "service": "Nova AI Agent"
+    })
+
+
+@app.route("/agent", methods=["post"])
+def agent():
+
+    try:
+        data = request.get_jsonify({
+            "success": False,
+            "message": "command is required"
+        }), 400
+
+if not is email_command(command):
+    return jsonify({
+        "success": False,
+        "message": "please give a gmail command."
+    }), 400
+
+recipient = exact_email(command)
+
+email = generate_email_with_gemini(command)
+
+return jsonify({
+    "susses": True,
+    "type": "email",
+    "email_generated": True,
+    "recipient": recipient,
+    "subject": email["subject"],
+    "body":email["body"],
+    "gmail_url": create_gmail_url(
+        email["subject"],
+        email["body"]'
+        recipient
+    )
+})
+
+except Exception as e:
+
+    return jsonify({
+        "success": False,
+        "message": str(e)
+    }), 500
+return app
+        
